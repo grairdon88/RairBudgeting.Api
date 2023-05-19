b99a9d4a188a8235595ba09e3a383ab8f182c445
Greg Rairdon <grairdon88@gmail.com>
2023-05-19 11:07:47 -0400

    Publishing an update.

--- RairBudgeting.Api/Program.cs
+++ RairBudgeting.Api/Program.cs
@@ -47,11 +47,13 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 else {
     if (!app.Environment.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
         app.UseSwagger();
 
         app.UseSwaggerUI(options =>
