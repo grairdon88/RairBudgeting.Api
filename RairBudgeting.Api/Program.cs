cf7948efa191e8220073d6861493c3b17d8314cc
Greg Rairdon <grairdon88@gmail.com>
2023-05-19 11:52:49 -0400

    Removed code

--- RairBudgeting.Api/Program.cs
+++ RairBudgeting.Api/Program.cs
@@ -40,10 +40,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-//using (var scope = app.Services.CreateScope()) {
-//    var context = scope.ServiceProvider.GetRequiredService<BudgetContext>();
-//    DBInitializer.Init(context);
-//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
