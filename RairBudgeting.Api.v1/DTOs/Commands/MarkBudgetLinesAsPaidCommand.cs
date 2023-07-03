fe754c8b1cc1f4e31bcb53662cff4b7fb7d60d4f
Greg Rairdon <grairdon88@gmail.com>
2023-07-02 21:50:00 -0400

    Fixing bugs

--- RairBudgeting.Api.v1/DTOs/Commands/MarkBudgetLinesAsPaidCommand.cs
+++ RairBudgeting.Api.v1/DTOs/Commands/MarkBudgetLinesAsPaidCommand.cs
@@ -9,5 +9,4 @@ namespace RairBudgeting.Api.v1.DTOs.Commands;
 public class MarkBudgetLinesAsPaidCommand : IRequest<Budget> {
     public Guid BudgetId { get; set; }
     public List<Guid> BudgetLineIds { get; set; }
-    public string UserId { get; set; }
 }
